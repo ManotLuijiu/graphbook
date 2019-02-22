443abaa6b3f39871fd25105cb3a32c132409135b
Manot Luijiu <manot.luijiu@gmail.com>
2019-02-22 20:46:44 +0700

    adding more dependency

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,28 @@
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 const root = path.join(__dirname, '../../');
 
 const app = express();
 const PORT = '8000';
+
+app.use(compress());
+
+app.use(cors());
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc:["'self'"],
+    scriptSrc:["'self'", "'unsafe-inline'"],
+    styleSrc:["'self'", "'unsafe-inline'"],
+    imgSrc:["'self'", "data:", "*.amazonaws.com"]
+  }
+}));
+app.use(helmet.referrerPolicy({policy:'same-origin'}));
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
